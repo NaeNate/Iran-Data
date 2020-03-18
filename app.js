7c5e51eac5e5427cd0649869516716aed099a56a
NaeNate <nate@qstreet.org>
2020-03-17 22:37:10 -0400

    Update URL to historical

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 const fs = require("fs")
 const request = require("request")
 const id = "6db149d34b424c659d4405257d108aeb"
+const yr = process.argv[2]
+const mm = process.argv[3]
+const dd = process.argv[4]
 
 const dataFind = () => {
-  const url = "https://openexchangerates.org/api/latest.json?app_id=" + id
+  const url = "https://openexchangerates.org/api/historical/" + yr + "-" + mm + "-" + dd + ".json?app_id=" + id
 
   request({ url, json: true }, (error, response) => {
     if (error) {
       console.log('Error: ' + error)
     } else {
       const date = new Date(Date.UTC(process.argv[2], process.argv[3], process.argv[4]));
-      // const date = dateGiven.getTime() / 1000
-
       const dataDay = date.getFullYear() + '-' + ("0" + date.getMonth()).slice(-2) + '-' + ("0" + date.getDate()).slice(-2)
       fs.appendFile('./data.txt', dataDay + ' ' + response.body.rates.VEF + '\n' , (e) => {
         if (e) {
