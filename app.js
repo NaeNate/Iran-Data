6f4826ac685e05b1f53e92070d9032628848c0a0
NaeNate <nate@qstreet.org>
2020-03-19 22:15:53 -0400

    Create Monster Function

--- app.js
+++ app.js
@@ -3,43 +3,78 @@ const request = require('request')
 const jsDay = 86400000
 const id = "6db149d34b424c659d4405257d108aeb"
 const year = process.argv[2]
-const month = process.argv[3]
+const month = process.argv[3] // 01 === Feb
 const day = process.argv[4]
-const daysAgo = process.argv[5]
+const daysAgo = process.argv[5] || 3
+const currency = process.argv[6] || "VEF"
 
 const convertDateToJSDateObject = function () {
   const date = new Date(Date.UTC(year, month, day))
-  console.log({date})
+  // console.log({date})
   return date
 }
 
 const convertJSDateObjectToJSTimestamp = function () {
   const timestampForGivenDay = convertDateToJSDateObject()
 .getTime()
-  console.log(timestampForGivenDay)
+  // console.log(timestampForGivenDay)
   return timestampForGivenDay
 }
 
 const findTimestampForEarliestDay = function () {
   const timestampForEarliestDay = convertJSDateObjectToJSTimestamp() - (daysAgo * jsDay)
-  console.log(timestampForEarliestDay)
+  // console.log(timestampForEarliestDay)
   return timestampForEarliestDay
 }
 
-const convertEarliestDayTimestampToDate = function () {
-  const dateOfEarliestDay = new Date(findTimestampForEarliestDay())
-  console.log(dateOfEarliestDay)
-  return dateOfEarliestDay
+const convertToJSDateObject = function (JSTimestamp) {
+  const javascriptDateObject = new Date(JSTimestamp)
+  // console.log(javascriptDateObject)
+  return javascriptDateObject
 }
 
+const pullDatesFromJSDateObject = function (JSdateObj) {
+  const date = JSdateObj.getFullYear() + '-' + ("0" + JSdateObj.getMonth()).slice(-2) + '-' + ("0" + JSdateObj.getDate()).slice(-2)
+  return date
+}
 
+const requestData = function (date) {
+  // console.log(typeof date)
+  const url = "https://openexchangerates.org/api/historical/" + date + ".json?app_id=" + id
+
+  request({ url, json: true }, (e, response) => {
+    if (e) {
+      console.log('Error: ' + e)
+    } else {
+      console.log(response.body.rates.VEF)
+    }
+  })
+}
+
+function monster(){
 for (var i = 0; i < daysAgo; i++) {
-  const n = convertEarliestDayTimestampToDate();
-  // we worked up to this point 
-  // Stuck on loop to call each day for api
-  
+
+  // up to this point
+  console.log(i)
 
 }
+}
 
-convertEarliestDayTimestampToDate()
 
+monster()
+
+// convertToJSDateObject(1575158400000)
+// requestData(pullDatesFromJSDateObject(convertToJSDateObject(1574726400000)))
+
+// convertEarliestDayTimestampToJSDateObject()
+// convertDateToJSDateObject()
+
+/*
+CLI app.js 2019 03 10 5 VEF
+Convert CLI data to a JSdateObj
+Convrt JSdObj into JS Timestamp
+subtract (daysAgo * jsDay) from JS Timestamp to get earliest JSTimestamp
+
+Monster Function: take in javascript timestamp, turn the timestamp into a javascript date object, pull the year, month, and day from the object, request from the api, append data to text file, 
+
+*/
