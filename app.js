cdf990f1c4b1860245a808ff228aa17bc695c1a1
NaeNate <nate@qstreet.org>
2020-03-20 23:48:15 -0400

    Make monster function.

--- app.js
+++ app.js
@@ -1,67 +1,102 @@
-const fs = require('fs')
-const request = require('request')
+const fs = require("fs")
+const request = require("request")
 const jsDay = 86400000
 const id = "6db149d34b424c659d4405257d108aeb"
+const today = new Date()
+let flag = false
 const year = process.argv[2]
 const month = process.argv[3] // 01 === Feb
 const day = process.argv[4]
 const daysAgo = process.argv[5] || 3
-const currency = process.argv[6] || "VEF"
+const currency = process.argv[6] || "GBP"
 
-const convertDateToJSDateObject = function () {
-  const date = new Date(Date.UTC(year, month, day))
-  // console.log({date})
-  return date
+console.log(today)
+
+const convertDateToJSDateObject = function() {
+  const JSDateObject = new Date(year, month, day)
+  return JSDateObject
 }
 
-const convertJSDateObjectToJSTimestamp = function () {
-  const timestampForGivenDay = convertDateToJSDateObject()
-.getTime()
-  // console.log(timestampForGivenDay)
+const convertJSDateObjectToJSTimestamp = function() {
+  const timestampForGivenDay = convertDateToJSDateObject().getTime()
   return timestampForGivenDay
 }
 
-const findTimestampForEarliestDay = function () {
-  const timestampForEarliestDay = convertJSDateObjectToJSTimestamp() - (daysAgo * jsDay)
-  // console.log(timestampForEarliestDay)
+const findTimestampForEarliestDay = function() {
+  const timestampForEarliestDay =
+    convertJSDateObjectToJSTimestamp() - (daysAgo - 1) * jsDay
   return timestampForEarliestDay
 }
 
-const convertToJSDateObject = function (JSTimestamp) {
+const convertToJSDateObject = function(JSTimestamp) {
   const javascriptDateObject = new Date(JSTimestamp)
-  // console.log(javascriptDateObject)
   return javascriptDateObject
 }
 
-const pullDatesFromJSDateObject = function (JSdateObj) {
-  const date = JSdateObj.getFullYear() + '-' + ("0" + JSdateObj.getMonth()).slice(-2) + '-' + ("0" + JSdateObj.getDate()).slice(-2)
-  return date
+const pullDatesFromJSDateObject = function(JSdateObj) {
+  const date =
+    JSdateObj.getFullYear() + "-" + ("0" + JSdateObj.getMonth()).slice(-2) + "-" + ("0" + JSdateObj.getDate()).slice(-2)
+    return date
 }
 
-const requestData = function (date) {
-  // console.log(typeof date)
-  const url = "https://openexchangerates.org/api/historical/" + date + ".json?app_id=" + id
+const requestData = function(timeStamp) {
+  // date is a jsTimestamp
+  // need to convert to jsDateObject
+  var JSdateObject = convertToJSDateObject(timeStamp)
+  var date = pullDatesFromJSDateObject(JSdateObject)
+
+  const url =
+    "https://openexchangerates.org/api/historical/" + date + ".json?app_id=" + id
 
   request({ url, json: true }, (e, response) => {
     if (e) {
-      console.log('Error: ' + e)
+      console.log("Error: " + e)
     } else {
-      console.log(response.body.rates.VEF)
+      console.log(response.body.rates.GBP)
+      const fileString = (date + ' ' + response.body.rates.GBP + '\n')
+
+      // fs.appendFile('./data.txt', date + ' ' + response.body.rates.GBP + '\n', (e) => {
+      fs.appendFile('./data.txt', fileString, (e) => {
+
+        if (e) {
+          console.log(e)
+        } else {
+          console.log('Data was entered')
+        }
+      })
+
+
+
+
+
     }
   })
 }
 
-function monster(){
-for (var i = 0; i < daysAgo; i++) {
-
-  // up to this point
-  console.log(i)
-
+function monster() {
+  let timeStampEarliestDay = findTimestampForEarliestDay()
+  
+  for (var i = 0; i < daysAgo; i++) {
+    requestData(timeStampEarliestDay)
+    // console.log(timeStampEarliestDay)
+    timeStampEarliestDay = timeStampEarliestDay + jsDay
+  }
 }
-}
-
 
 monster()
+
+
+/*
+// 'a' flag stands for 'append'
+const log = fs.createWriteStream('log.txt', { flags: 'a' });
+
+// on new log entry ->
+log.write('new entry\n');
+
+// you can skip closing the stream if you want it to be opened while
+// a program runs, then file handle will be closed
+log.end();
+*/
 
 // convertToJSDateObject(1575158400000)
 // requestData(pullDatesFromJSDateObject(convertToJSDateObject(1574726400000)))
